7008de81613648bc4691f12b12175b046a74ea44
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-10-23 00:05:55 +0530

    feat: add Redux setup with counter slice and store configuration

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+/* 
+To Setup Redux install the following packages:
+- npm install @reduxjs/toolkit react-redux
+- Create a state folder with slices and store files
+- Wrap the <App /> component with <Provider store={store}> in this file
+- Use useSelector and useDispatch hooks in your components to interact with the Redux store
+*/
+
+import store from './state/store.ts'
+import { Provider } from 'react-redux'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 )
