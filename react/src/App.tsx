82369035164b00b9644121dfa65faac4d9855ba6
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-11-05 00:11:24 +0530

    feat: implement loading and error display components

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,17 +1,20 @@
+import ErrorDisplay from "./components/ErrorDisplay";
+import Loading from "./components/Loading";
 import PostList from "./components/PostList";
 import { usePosts } from "./hooks/posts.hook";
 
 function App() {
   /* Single Responsibility for this Component:
        - Get all posts using hooks
-       - Show the Home Page (Handle Error and Loading States)
+       - Show the Home Page
+       - (Handle Error and Loading States) in separate components
   */
   const { data, isLoading, error } = usePosts();
   return (
     <div className="text-center space-y-8">
       <h1 className="text-3xl font-bold">Posts</h1>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error fetching posts</p>}
+      {isLoading && <Loading />}
+      {error && <ErrorDisplay />}
       {data && data.length > 0 && (
         <PostList posts={data} />
       )}
---
+++ react/src/components/ErrorDisplay.tsx
@@ -0,0 +1,7 @@
+export default function ErrorDisplay() {
+    return (
+        <div className="text-center text-red-500">
+            <p>Error fetching posts</p>
+        </div>
+    )
+}
