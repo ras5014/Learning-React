3d7e8289b991b1b9f1e84b5eeecb4c2dce0b997f
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2026-01-09 14:08:40 +0530

    feat: initialize products microfrontend with React and Webpack
    - Added package.json for dependencies and scripts
    - Created index.html for the application entry point
    - Implemented ProductList component to display a list of products
    - Developed Products component to manage product data and render ProductList
    - Added main.jsx as the application entry file to render Products component
    - Configured Webpack with Module Federation for microfrontend architecture
    - Included CSS support in Webpack configuration

---
+++ react/products/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.jsx",
+  devtool: "source-map",
+  devServer: {
+    port: 3002,
+  },
+  output: {
+    publicPath: "http://localhost:3002/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Products": "./src/Products.jsx",
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        "react-dom": { singleton: true, eager: true },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
